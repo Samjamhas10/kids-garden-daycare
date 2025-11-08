f6d4a9accb7fca064695c71b02df16a313a313e9
SamSam Hashi <sammymadobe@gmail.com>
2025-11-08 17:28:32 -0600

    WIP: add more info to the contact section and style it

--- src/components/ContactSection/ContactSection.jsx
+++ src/components/ContactSection/ContactSection.jsx
@@ -3,10 +3,38 @@ import "./ContactSection.css";
 function ContactSection() {
   return (
     <section id="contact" className="contact">
-      <h2>Contact Us</h2>
-      <p>Email: kidsgardendaycare959@gmail.com</p>
-      <p>Phone: (651) 493-4951</p>
-      <p>Address: 959 W. Minnehaha Ave St. Paul, MN 55104</p>
+      <div className="contact__intro">
+        <h2 className="contact__heading">
+          Let's start your child's journey today!
+        </h2>
+        <p className="contact__subheading">
+          Ready to explore what we have to offer?
+        </p>
+
+        <div className="contact__item">
+          <h3>Visit Us</h3>
+          <address>
+            959 W. Minnehaha Ave
+            <br />
+            St. Paul, MN 55104
+          </address>
+        </div>
+
+        <div className="contact__item">
+          <h3>Call Us</h3>
+          <p>📞 (651) 493-4951</p>
+        </div>
+
+        <div className="contact__item">
+          <h3>Email Us</h3>
+          <p>📧 kidsgardendaycare959@gmail.com</p>
+        </div>
+
+        <div className="contact__item">
+          <h3>Daycare Hours</h3>
+          <p>Mon - Sun: 7:00AM to 9:30PM</p>
+        </div>
+      </div>
     </section>
   );
 }
