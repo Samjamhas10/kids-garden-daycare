86885bbb5669ef729d76e2bbb93316c8c55115b5
SamSam Hashi <sammymadobe@gmail.com>
2025-11-08 16:56:00 -0600

    Implement and style footer with daycare info, navigation links, and social media

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,44 +3,54 @@ import "./Footer.css";
 function Footer() {
   return (
     <footer className="footer">
-      <section id="info" className="footer__info">
-        <p>Address: 959 W. Minnehaha Ave St. Paul, MN 55104</p>
-        <p>Phone: (651) 493-4951</p>
-        <p>Email: kidsgardendaycare959@gmail.com</p>
-      </section>
-      <div>
-        <p className="footer__links">Quick Links</p>
-        <ul>
-          <li>
-            <a href="#home">Home</a>
-          </li>
-          <li>
-            <a href="#about">About</a>
-          </li>
-          <li>
-            <a href="#programs">Programs</a>
-          </li>
-          <li>
-            <a href="#schedule">Schedule</a>
-          </li>
-          <li>
-            <a href="#contact">Contact</a>
-          </li>
-        </ul>
+      <div className="footer__container">
+        <div className="footer__section">
+          <h3 className="footer__title">Contact Us</h3>
+          <p>
+            959 W. Minnehaha Ave
+            <br />
+            St. Paul, MN 55104
+          </p>
+          <p>📞 (651) 493-4951</p>
+          <p>📧 kidsgardendaycare959@gmail.com</p>
+        </div>
+        <div className="footer__section">
+          <h3 className="footer__title">Quick Links</h3>
+          <ul className="footer__list">
+            <li>
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#programs">Programs</a>
+            </li>
+            <li>
+              <a href="#schedule">Schedule</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+        <div className="footer__section">
+          <h3 className="footer__title">Our Programs</h3>
+          <ul className="footer__list">
+            <li>Infants (0 - 12 months)</li>
+            <li>Toddlers (12 months - 3 years)</li>
+            <li>Preschool (3 - 5 years)</li>
+            <li>Pre-K (5 - 7 years)</li>
+            <li>School Age (7 - 13 years)</li>
+          </ul>
+        </div>
       </div>
+
       <div>
-        <p className="footer__programs">Our Programs</p>
-        <ul>
-          <li className="footer__list">Infants (0 - 12 months)</li>
-          <li className="footer__list">Toddlers (12 months - 3 years)</li>
-          <li className="footer__list">Preschool (3 years - 5 years)</li>
-          <li className="footer__list">Pre-K (5 years - 7 years)</li>
-          <li className="footer__list">Schoolage (7 years - 13 years)</li>
-        </ul>
+        <p className="footer__copyright">
+          @2025 Kids Garden Daycare. All Rights Reserved.
+        </p>
       </div>
-      <p className="footer__copyright">
-        @2025 Kids Garden Daycare. All Rights Reserved.
-      </p>
     </footer>
   );
 }
