ad342d326f04532da141bdc43439a92b8aed5437
SamSam Hashi <sammymadobe@gmail.com>
2025-11-10 20:09:15 -0600

    Add logo and style the navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,14 @@
+import logo from "../../assets/logo.png";
 import "./Navbar.css";
 
 function Navbar() {
   return (
     <nav className="navbar">
-      <ul>
+      <div className="navbar__logo">
+        <img src={logo} alt="Kids Garden Daycare logo" className="logo" />
+        {/* <h1>Kids Garden Daycare</h1> */}
+      </div>
+      <ul className="navbar__links">
         <li>
           <a href="#home">Home</a>
         </li>
@@ -20,6 +25,11 @@ function Navbar() {
           <a href="#contact">Contact</a>
         </li>
       </ul>
+      <div className="navbar__cta">
+        <a href="#enroll" className="navbar__button">
+          Enroll Now
+        </a>
+      </div>
     </nav>
   );
 }
