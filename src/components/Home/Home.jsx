6128f0aaa85af8f1811d8c9b2c6e5e3c627d9491
SamSam Hashi <sammymadobe@gmail.com>
2025-11-12 16:11:41 -0600

    WIP: add some context and style home section

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,14 @@
 import "./Home.css";
 
 function Home() {
-  return <></>;
+  return (
+    <section id="home" className="home">
+      <div className="home__content">
+        <h1>Welcome To Kids Garden Daycare</h1>
+        <h3>Where Learning and Fun Go Hand In Hand</h3>
+      </div>
+    </section>
+  );
 }
 
 export default Home;
